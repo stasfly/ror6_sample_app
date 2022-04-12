809963cd1c4bea659fba2c3ffbed170e855c7b9a
stasfly <s19f84@gmail.com>
2022-04-12 22:31:22 +0300

    Implement advsnced login.  End of ch.9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
       reset_session
-      log_in user
-      redirect_to user
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
@@ -16,9 +17,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
-    
+    log_out if logged_in?
     flash[:success] = "You have successfully logout."
     redirect_to root_url
   end
 end
+
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -32,9 +32,31 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    # Simulate a user clicking logout in a second window.
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
+  
+        def log_in_as(user, password: "password", remember_me: '1')
+          post login_path, params: {session: {email: user.email,
+            password: password, remember_me: remember_me } }
+        end
+  
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    #assert_not_empty cookies[:remember_token] 
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
+  end
+
+  test 'login without remembering' do
+    #login to set the cookie.
+    log_in_as(@user, remember_me: "1")
+    #login and verify that cookie is deleted
+    log_in_as(@user, remember_me: "0")
+    assert cookies[:remember_token].blank?
+  end
+
 end
